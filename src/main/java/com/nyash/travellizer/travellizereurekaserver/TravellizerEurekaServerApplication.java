4e9373138cca88c7b2378b3674a5ac0eb8900bad
Nyaashmyaash <81423982+Nyaashmyaash@users.noreply.github.com>
2021-07-06 02:41:16 +0700

    Initial commit

---
+++ src/main/java/com/nyash/travellizer/travellizereurekaserver/TravellizerEurekaServerApplication.java
@@ -0,0 +1,13 @@
+package com.nyash.travellizer.travellizereurekaserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TravellizerEurekaServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TravellizerEurekaServerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/nyash/travellizer/travellizereurekaserver/TravellizerEurekaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nyash.travellizer.travellizereurekaserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TravellizerEurekaServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
