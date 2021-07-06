9f0e714ba35b019628d552870c9523e443ad1e27
Nyaashmyaash <81423982+Nyaashmyaash@users.noreply.github.com>
2021-07-06 21:59:11 +0700

    add changes

--- src/main/java/com/nyash/travellizer/travellizereurekaserver/TravellizerEurekaServerApplication.java
+++ src/main/java/com/nyash/travellizer/travellizereurekaserver/TravellizerEurekaServerApplication.java
@@ -2,8 +2,10 @@ package com.nyash.travellizer.travellizereurekaserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
+@EnableEurekaServer
 public class TravellizerEurekaServerApplication {
 
     public static void main(String[] args) {
